fba9972b53f02df14384ca9bc00711ed89483b10
QuickGiveMeMemes <quickgivemememes@gmail.com>
2021-04-29 21:37:09 -0700

    gseirfdhjhudbhi

--- dashboardProjectSystem.js
+++ dashboardProjectSystem.js
@@ -8,10 +8,10 @@ function closeModal() {
 }
 document.getElementById('newproject').addEventListener('click',e=>createModal());
 document.getElementById('closemodal').addEventListener('click',e=>closeModal());
-document.getElementById('modalbackground').addEventListener('click',e=>closeModal());
+//document.getElementById('modalbackground').addEventListener('click',e=>closeModal());
 
 //have rowbox which is a counter for boxes on each row, max 4 boxes on each row max 8 projects, indexes store all info
-/*
+//JSON.stringify and json.parse
 function findSubjects(subjectarray) {
     subjectarray.forEach(subject=>{
         var newoption = document.createElement("option");
@@ -21,11 +21,19 @@ function findSubjects(subjectarray) {
     })
     if(subjectarray.length == 0) {
         var newoption = document.createElement("option");
-        newoption.value = none;
-        newoption.innerText = none;
+        newoption.value = 'No Subjects';
+        newoption.innerText = 'No Subjects';
         document.getElementById('subject').appendChild(newoption);
     }
 }
+/*
+var row1=0;
+var row2=0;
+localStorage.setItem('row1',row1);
+var placeholder = [];
+localStorage.setItem('subjectarray',JSON.stringify(placeholder))
+var subjectarray = JSON.parse(localStorage.getItem('subjectarray'));
+document.getElementById('newproject').addEventListener('click',e=>findSubjects(subjectarray));
 function addNewProject(row1,row2) {
     if(row1+row2>8) {
         alert("Error: you can have a maximum of 8 projects at a time.");
@@ -37,9 +45,8 @@ function addNewProject(row1,row2) {
     }
     //stuff will go here later
 }
-document.getElementById('newprojectbutton').addEventListener('click',addNewProject(row1,row2));
+
+document.getElementById('submitbutton').addEventListener('click',addNewProject(row1,row2));
+
+
 */
-
-
-
-
