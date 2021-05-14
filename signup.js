25752ef83ec5bcf9b706ec9cb590b40842025f27
QuickGiveMeMemes <quickgivemememes@gmail.com>
2021-05-13 20:23:11 -0700

    Update signup.js

--- signup.js
+++ signup.js
@@ -2,8 +2,12 @@ function addNewUser() {
     var username = document.getElementById('user').value;
     var password = document.getElementById('pass').value;
     var email = document.getElementById('mail').value;
-    var newUserArray={'username':username,'password':password,'email':email};
-    localStorage.setItem('user',JSON.stringify(newUserArray));
-    window.location='dashboard.html'
+    if(username==''||password==''||email=='') {
+        alert('One/Some of these input boxes are empty');
+    } else {
+        var newUserArray={'username':username,'password':password,'email':email};
+        localStorage.setItem('user',JSON.stringify(newUserArray));
+        window.location='dashboard.html'
+    }
 }
 document.getElementById('signup').addEventListener('click',e=>addNewUser());
