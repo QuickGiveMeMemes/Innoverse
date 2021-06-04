20bd9366761f5014f3204e06e7c6dac5410ec68d
QuickGiveMeMemes <quickgivemememes@gmail.com>
2021-06-04 14:24:52 -0700

    poihoh

--- dashboardProjectSystem.js
+++ dashboardProjectSystem.js
@@ -26,27 +26,7 @@ function findSubjects(subjectarray) {
         document.getElementById('subject').appendChild(newoption);
     }
 }
-/*
-var row1=0;
-var row2=0;
-localStorage.setItem('row1',row1);
-var placeholder = [];
-localStorage.setItem('subjectarray',JSON.stringify(placeholder))
-var subjectarray = JSON.parse(localStorage.getItem('subjectarray'));
-document.getElementById('newproject').addEventListener('click',e=>findSubjects(subjectarray));
-function addNewProject(row1,row2) {
-    if(row1+row2>8) {
-        alert("Error: you can have a maximum of 8 projects at a time.");
-        return;
-    } else if(row1>=4) {
-        row2+=1;
-    } else {
-        row1+=1;
-    }
-    //stuff will go here later
+
+function createProject() {
+    
 }
-
-document.getElementById('submitbutton').addEventListener('click',addNewProject(row1,row2));
-
-
-*/
