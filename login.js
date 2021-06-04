635b5797cba0075995ada569635b498efeba2fc7
BeTheFuture <bethefuture999@gmail.com>
2021-06-04 14:55:56 -0700

    Change hover button for "New project", tested features, all of them work

--- login.js
+++ login.js
@@ -1,17 +1,17 @@
 function login() {
-    var anyWrong = false
+    var anyWrong = false;
     var username = document.getElementById('user').value;
     var password = document.getElementById('pass').value;
     var email = document.getElementById('mail').value;
     var account = JSON.parse(localStorage.getItem('user'));
     if (username!=account['username'] || password!=account['password'] || email!=account['email']) {
-        anyWrong = true
+        anyWrong = true;
     }
     if (anyWrong==false) {
-        localStorage.setItem('signedIn','True')
-        window.location='dashboard.html'
+        localStorage.setItem('signedIn','True');
+        window.location='dashboard.html';
     } else {
-        alert('Wrong Username or Password or Email')
+        alert('Wrong Username or Password or Email');
     }
 }
-document.getElementById('signup').addEventListener('click',e=>login())
+document.getElementById('signup').addEventListener('click',e=>login());
