b4e85aa9cfe629cb26fe4ae5192619eee29803b5
QuickGiveMeMemes <quickgivemememes@gmail.com>
2021-07-01 21:06:06 -0700

    Major Changes

--- course-overview/baking.js
+++ course-overview/baking.js
@@ -1,1 +1,10 @@
+function addBakingCourse() {
+    var courses=JSON.parse(localStorage.getItem('courseArray'));
+    courses.push('Baking')
+    localStorage.setItem('courseArray',JSON.stringify(courses));
+}
+if(localStorage.getItem('projects')==null) {
+    localStorage.setItem('projects',[JSON.stringify([['Baking','Baking','baking descrition']])])
+}
 localStorage.setItem('currentpage','baking.html')
+document.getElementById('buybutton').addEventListener('click',e=>addBakingCourse());
--- dashboard.js
+++ dashboard.js
@@ -1,4 +1,37 @@
 //modal javascript
+function newChoice(subject) {
+    var newoption = document.createElement("option");
+    newoption.value = subject;
+    newoption.innerText = subject;
+    document.getElementById('subject').appendChild(newoption);
+}
+function addProject() {
+    var projectlist=JSON.parse(localStorage.getItem('projects'));
+    var title=document.getElementById('projtitle').value;
+    var subject=document.getElementById('subject').value;
+    var desc=document.getElementById('projdesc').value;
+    projectlist.push([title,subject,desc]);
+    localStorage.setItem('projects',JSON.stringify(projectlist));
+    location.reload();
+}
+function displayProject(info) {
+    var box = document.createElement("div");
+    var text=document.createElement('p');
+    var detailsbtn=document.createElement('button');
+    var deletebtn=document.createElement('button');
+    text.innerHTML=info[0]+' ('+info[1]+') - <span class=projectboxspan>'+info[2]+'</span>';
+    detailsbtn.innerHTML='Details'
+    deletebtn.innerHTML='Delete'
+    box.classList.add('projectbox');
+    text.classList.add('projectboxtext');
+    detailsbtn.classList.add('detailsbtn');
+    deletebtn.classList.add('deletebtn');
+    box.appendChild(text);
+    box.appendChild(detailsbtn);
+    box.appendChild(deletebtn);
+    document.getElementById('projectrow').appendChild(box);
+    console.log('foo')
+}
 function createModal() {
     document.getElementById('modalbackground').style.display="block";
     console.log('foo');
@@ -6,24 +39,21 @@ function createModal() {
 function closeModal() {
     document.getElementById('modalbackground').style.display="none";
 }
+function findSubjects(subjectarray) {
+    subjectarray.forEach(newChoice);
+}
+function findProjects(projects) {
+    projects.forEach(displayProject);
+}
 document.getElementById('newproject').addEventListener('click',e=>createModal());
+document.getElementById('newproject').addEventListener('click',e=>findSubjects(JSON.parse(localStorage.getItem('courseArray'))));
 document.getElementById('closemodal').addEventListener('click',e=>closeModal());
-localStorage.setItem('currentpage','dashboard.html')
+document.getElementById('submitbutton').addEventListener('click',e=>addProject());
+findProjects(JSON.parse(localStorage.getItem('projects')));
+if(localStorage.getItem('projects')==null) {
+    localStorage.setItem('projects',[JSON.stringify([['Baking','Baking','baking descrition']])])
+}
 //document.getElementById('modalbackground').addEventListener('click',e=>closeModal());
 
 //have rowbox which is a counter for boxes on each row, max 4 boxes on each row max 8 projects, indexes store all info
 //JSON.stringify and json.parse
-function findSubjects(subjectarray) {
-    subjectarray.forEach(subject=>{
-        var newoption = document.createElement("option");
-        newoption.value = subject;
-        newoption.innerText = subject;
-        document.getElementById('subject').appendChild(newoption);
-    })
-    if(subjectarray.length == 0) {
-        var newoption = document.createElement("option");
-        newoption.value = 'No Subjects';
-        newoption.innerText = 'No Subjects';
-        document.getElementById('subject').appendChild(newoption);
-    }
-}
